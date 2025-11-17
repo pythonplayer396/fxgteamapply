56cf59c706ad28540e1e04bf7320feabb2e1597e
pythonplayer396 <roshw0023@gmail.com>
2025-11-17 12:51:18 -0500

    Fix JSX syntax error in film grain overlay
    - Move SVG data URL from className to inline style
    - Properly escape quotes in SVG markup
    - This should resolve the rendering issues preventing the Render-style design from displaying

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,9 @@ export default function Home() {
           <div className="render-grid"></div>
           <div className="grid-sweep"></div>
           {/* Film grain overlay */}
-          <div className="absolute inset-0 opacity-[0.015] bg-[url('data:image/svg+xml,%3Csvg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"%3E%3Cfilter id="noiseFilter"%3E%3CfeTurbulence type="fractalNoise" baseFrequency="0.9" numOctaves="4" stitchTiles="stitch"/%3E%3C/filter%3E%3Crect width="100%25" height="100%25" filter="url(%23noiseFilter)"/%3E%3C/svg%3E')]"></div>
+          <div className="absolute inset-0 opacity-[0.015]" style={{
+            backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`
+          }}></div>
         </div>
 
         {/* Floating Dashboard Panels */}
